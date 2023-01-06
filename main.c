6dfc4a0e85a3094f5431274608015c137595b059
farukdll <sefoni8668@gmail.com>
2023-01-06 16:46:32 +0300

    philo

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpektas <fpektas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 16:02:14 by fpektas           #+#    #+#             */
+/*   Updated: 2022/11/09 16:02:14 by fpektas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	t_philo			*philo;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*death;
+
+	if (ft_argcheck(argc, argv) == ERROR)
+	{
+		printf("invalid argument");
+		return (1);
+	}
+	philo = malloc(sizeof(*philo) * ft_atol(argv[1]));
+	forks = malloc(sizeof(pthread_mutex_t) * ft_atol(argv[1]));
+	death = malloc(sizeof(pthread_mutex_t));
+	if (!philo || !forks || !death)
+		return (ERROR);
+	ft_philo_init(philo, argv, argc);
+	create_init_mutex(philo, forks, death);
+	ft_distribute_threads(philo, argv);
+	ft_free(philo, forks, death);
+	return (0);
+}
